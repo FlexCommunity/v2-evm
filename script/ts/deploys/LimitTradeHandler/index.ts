90ef2a90d27cc0cd84b15e4edc1de6c47b00324a
FlexDev <flexdev@flex.community>
2024-05-10 13:55:17 -0300

    deploy limitTradeHandler

--- script/ts/deploys/LimitTradeHandler/index.ts
+++ script/ts/deploys/LimitTradeHandler/index.ts
@@ -1,11 +1,12 @@
-import { ethers, tenderly, upgrades, network } from "hardhat";
+import { ethers, run, tenderly, upgrades, network } from "hardhat";
 import { getConfig, writeConfigFile } from "../../utils/config";
 import { getImplementationAddress } from "@openzeppelin/upgrades-core";
 
 const BigNumber = ethers.BigNumber;
 const config = getConfig();
 
-const minExecutionFee = ethers.utils.parseEther("0.0003"); // 0.0003 ether
+// const minExecutionFee = ethers.utils.parseEther("0.0003"); // 0.0003 ether
+const minExecutionFee = ethers.utils.parseEther("0.001"); // 0.001 ETH
 const minExecutionTimestamp = 60 * 60 * 5; // 5 minutes
 
 async function main() {
@@ -14,7 +15,10 @@ async function main() {
   const Contract = await ethers.getContractFactory("LimitTradeHandler", deployer);
   const contract = await upgrades.deployProxy(
     Contract,
-    [config.tokens.weth, config.services.trade, config.oracles.ecoPyth, minExecutionFee, minExecutionTimestamp],
+    [
+      config.tokens.weth, config.services.trade, config.oracles.ecoPyth2,
+      minExecutionFee, minExecutionTimestamp
+    ],
     {
       unsafeAllow: ["delegatecall"],
     }
@@ -27,9 +31,15 @@ async function main() {
   writeConfigFile(config);
 
   await tenderly.verify({
-    address: await getImplementationAddress(network.provider, contract.address),
+    address: await getImplementationAddress(network.provider, config.handlers.limitTrade),
     name: "LimitTradeHandler",
   });
+
+  await run("verify:verify", {
+    address: await getImplementationAddress(network.provider, config.handlers.limitTrade),
+    constructorArguments: [],
+  });
+
 }
 
 main().catch((error) => {
